a78d59bf203235ae4856c8cffabefd488e95c0a8
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-02-28 19:57:53 +0200

    Add Table Class

---
+++ src/main/java/Structures/Table.java
@@ -0,0 +1,15 @@
+package Structures;
+
+import java.util.Hashtable;
+
+public class Table {
+    private String TableName;
+    String ClusteringKeyColumn;
+    Hashtable<String, String> colNmTypes;
+
+
+    public Table(){
+
+    }
+
+}
