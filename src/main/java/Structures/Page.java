df80324ce54f5f382135916e69383ac138594cab
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-02-28 19:57:25 +0200

    Add Tuple , Page , Table Classes and implement the Tuple Class

---
+++ src/main/java/Structures/Page.java
@@ -0,0 +1,6 @@
+package Structures;
+
+public class Page {
+    private int maxNumOfRows;
+
+}
---
+++ src/main/java/Structures/Tuple.java
@@ -0,0 +1,33 @@
+package Structures;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+public class Tuple implements Serializable{
+    private HashMap<String,Object> values;
+
+    public Tuple(HashMap<String,Object> values){
+        this.values = values;
+    }
+    public Object getValue(String columnName) {
+        return values.get(columnName);
+    }
+
+    public void setValue(String columnName, Object value) {
+        values.put(columnName, value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (HashMap.Entry<String, Object> entry : values.entrySet()) {
+            sb.append(entry.getValue()).append(",");
+        }
+        // Remove the last comma
+        if (!values.isEmpty()) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        return sb.toString();
+    }
+
+}
