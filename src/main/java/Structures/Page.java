711d1658abe4da2e475681cb575cd753b24b1b4e
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-02-28 20:38:57 +0200

    Add Reading from DBApp.config and Create the Page Class

--- src/main/java/Structures/Page.java
+++ src/main/java/Structures/Page.java
@@ -1,6 +1,29 @@
 package Structures;
 
+import Structures.Tuple;
+import utils.Configurator;
+import java.util.Vector;
+
+
 public class Page {
     private int maxNumOfRows;
+    private Vector<Tuple> tuples;
 
+    public Page(){
+        this.maxNumOfRows = Configurator.getMaxPageSize();
+        System.out.println(maxNumOfRows);
+        tuples = new Vector<Tuple>();
+    }
+
+    public boolean isFull(){
+        return tuples.size() == maxNumOfRows;
+    }
+
+    public void addTuple(Tuple tuple){
+        tuples.add(tuple);
+    }
+
+    public Vector<Tuple> getTuples(){
+        return tuples;
+    }
 }
---
+++ src/main/java/utils/Configurator.java
@@ -0,0 +1,24 @@
+package utils;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Configurator {
+
+    public static Properties readFile(){
+        Properties properties = new Properties();
+        try (FileInputStream input = new FileInputStream("src/main/resources/DBApp.config")) {
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+
+    public static int getMaxPageSize(){
+        Properties properties = readFile();
+        return Integer.parseInt(properties.getProperty("MaximumRowsCountinPage"));
+    }
+
+
+}
