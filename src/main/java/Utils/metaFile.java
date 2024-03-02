b350c9f625c041793ef26ee9b6790b2e3d1baee8
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-03-02 18:36:29 +0200

    Add pom.xml dependencies and begin updateCSV

--- src/main/java/Utils/metaFile.java
+++ src/main/java/Utils/metaFile.java
@@ -1,12 +1,13 @@
 package Utils;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 
 public class metaFile {
 
@@ -18,6 +19,10 @@ public class metaFile {
         } else {
             try{
                 file.createNewFile();
+                String[] header = {"Table Name", "Column Name", "Column Type", "ClusteringKey", "IndexName", "IndexType"};
+                // Create FileWriter and CSVPrinter
+                try (FileWriter fileWriter = new FileWriter(file);
+                     CSVPrinter csvPrinter = new CSVPrinter(fileWriter, CSVFormat.DEFAULT.withHeader(header))) {} // inserts Table header
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -41,12 +46,34 @@ public class metaFile {
 
     }
 
+    public static <CSVReader> void updateOnMetaDataFile(String strTableName, String colName, String indexName, String indexType) {
+        String path = "./DB/metadata.csv";
+        try (
+                Reader reader = new FileReader(path);
+                Writer writer = new FileWriter(path, true); // Append mode to avoid overwriting existing data
+                CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader());
+                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT)
+        ) {
+            // Read existing records
+            for (CSVRecord csvRecord : csvParser) {
+                System.out.println(csvRecord);
+            }
+
+            // Add your update logic here
+
+            // Write updated records back to the file
+            csvPrinter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     public static void main(String[] args) {
-        Hashtable htblColNameType = new Hashtable( );
-        htblColNameType.put("id", "java.lang.Integer");
-        htblColNameType.put("name", "java.lang.String");
-        htblColNameType.put("gpa", "java.lang.double");
-        appendOnMetaDataFile("table1", "id",htblColNameType);
+        Hashtable<String, String> htblColNameType = new Hashtable<>();
+        htblColNameType.put("column1", "String");
+        htblColNameType.put("column2", "Integer");
+        updateOnMetaDataFile("tableName", "column1", "indexName", "indexType");
     }
 
 
